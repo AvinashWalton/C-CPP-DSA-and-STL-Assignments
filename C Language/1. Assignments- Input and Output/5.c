d207cb1d86de69bd111aad368e92e2428514dba9
Sonu Suman <sonukumarsuman081@gmail.com>
2024-03-09 10:22:00 +0530

    Add files via upload

---
+++ C Language/1. Assignments- Input and Output/5.c
@@ -0,0 +1,9 @@
+//Write a program to print \r on the screen.
+
+#include<stdio.h>
+int main()
+{
+    printf("\\r\n");
+
+    return(0);
+}
---
+++ C Language/1. Assignments- Input and Output/6.c
@@ -0,0 +1,9 @@
+//Write a program to print "Teacher's Day" on the screen. (Remember to print double and single quotes)
+
+#include<stdio.h>
+int main()
+{
+    printf("\"Teacher\'s Day\"\n");
+
+    return(0);
+}
