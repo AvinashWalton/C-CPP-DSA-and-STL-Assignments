1a17cd3c0cb394a00d773f63582adc93ca360102
Sonu Suman <sonukumarsuman081@gmail.com>
2024-03-12 09:17:55 +0530

    Add files via upload

---
+++ C Language/1. Assignments- Input and Output/Assign1_Solution_MYSIRG.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+//Q1
+/*
+int main()
+{
+    printf("Hello Students");
+    printf("\n");
+    return 0;
+}
+
+//Q2
+int main()
+{
+    printf("Hello\nStudents");
+    printf("\n");
+    return 0;
+}
+//Q3
+int main()
+{
+    printf("\"MySirG\"");
+    printf("\n");
+    return 0;
+}
+//Q4
+int main()
+{
+    printf("\\n");
+    printf("\n");
+    return 0;
+}
+
+//Q5
+int main()
+{
+    printf("\\r");
+    printf("\n");
+    return 0;
+}
+
+//Q6
+int main()
+{
+
+    printf("\"Teacher's Day\"");
+    printf("\n");
+    return 0;
+}
+
+//Q7
+int main()
+{
+    int a,b;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    printf("Sum of %d and %d is %d",a,b,a+b);
+    printf("\n");
+    return 0;
+}
+
+//Q8
+int main()
+{
+    int a;
+    printf("Enter a number");
+    scanf("%d",&a);
+    printf("Square of %d is %d",a,a*a);
+    printf("\n");
+    return 0;
+}
+
+//Q9
+int main()
+{
+    float l,b;
+    printf("Enter length and breadth of a rectangle");
+    scanf("%f%f",&l,&b);
+    printf("Area is %f",l*b);
+    printf("\n");
+    return 0;
+}
+*/
+//Q10
+int main()
+{
+    int r;
+    printf("Enter radius of a circle");
+    scanf("%d",&r);
+    printf("Area is %f",3.14*r*r);
+    printf("\n");
+    return 0;
+}
