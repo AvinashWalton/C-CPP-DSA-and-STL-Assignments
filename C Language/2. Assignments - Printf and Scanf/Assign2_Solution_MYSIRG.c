305c9d1fbf902eedb734ebf1729eba3e315391c0
Sonu Suman <sonukumarsuman081@gmail.com>
2024-03-16 10:31:16 +0530

    MySirG Assignments Solution

---
+++ C Language/2. Assignments - Printf and Scanf/Assign2_Solution_MYSIRG.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+/*
+//Q1
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("Average is %f",(a+b+c)/3.0);
+    printf("\n");
+    return 0;
+}
+
+//Q2
+int main()
+{
+    int r;
+    printf("Enter radius of a circle");
+    scanf("%d",&r);
+    printf("Circumference: %f",2*3.14*r);
+    printf("\n");
+    return 0;
+}
+
+//Q3
+int main()
+{
+    int p,t;
+    float r,si;
+    printf("Enter p, r and t");
+    scanf("%d%f%d",&p,&r,&t);
+    si=p*r*t/100;
+    printf("Simple Interest is %f",si);
+    printf("\n");
+    return 0;
+}
+
+//Q4
+int main()
+{
+    int l,b,h;
+    printf("Enter length, breadth and height of a cuboid");
+    scanf("%d%d%d",&l,&b,&h);
+    printf("Volume is %d",l*b*h);
+    printf("\n");
+    return 0;
+}
+*/
+
+//Q5
+int main()
+{
+    float cp,sp,p;
+    printf("Input cost price of 12 Bananas");
+    scanf("%f",&cp);
+    printf("Input Selling Price of 12 Bananas");
+    scanf("%f",&sp);
+    printf("Profit or Loss is %f",(sp-cp)/12*25);
+    printf("\n");
+    return 0;
+}
