f1199b602a8ec6e1e40be8b282122cf6e00f6ffa
Sonu Suman <sonukumarsuman081@gmail.com>
2024-03-16 10:32:13 +0530

    MySirG Assignments Solution

---
+++ C Language/3. Assignments - Printf and Scanf/Assign3_Solution_MYSIRG.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+/*
+//Q1
+int main()
+{
+    char ch;
+    printf("enter a character");
+    scanf("%c",&ch);
+    printf("%c - %d",ch,ch);
+    printf("\n");
+    return 0;
+}
+//Q2
+int main()
+{
+    int a;
+    printf("enter ascii code");
+    scanf("%d",&a);
+    printf("%d - %c",a,a);
+    printf("\n");
+    return 0;
+}
+
+//Q3
+int main()
+{
+    char a,b,c;
+    printf("enter three characters");
+    scanf("%c %c %c",&a,&b,&c);
+    printf("%c-%d\n%c-%d\n%c-%d",a,a,b,b,c,c);
+    printf("\n");
+    return 0;
+}
+
+//Q4
+int main()
+{
+    int x;
+    printf("enter a number");
+    scanf("%d",&x);
+    printf("Last digit of %d is %d",x,x%10);
+    printf("\n");
+    return 0;
+}*/
+//Q5
+int main()
+{
+    int x;
+    printf("enter a number");
+    scanf("%d",&x);
+    printf("Number without last digit of %d is %d",x,x/10);
+    printf("\n");
+    return 0;
+}
