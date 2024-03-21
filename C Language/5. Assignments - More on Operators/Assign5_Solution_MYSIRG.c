6a125a6dc709e334968511a10b9aeafc006e864f
Sonu Suman <sonukumarsuman081@gmail.com>
2024-03-21 13:31:32 +0530

    MySirG Assignments Solution

---
+++ C Language/5. Assignments - More on Operators/Assign5_Solution_MYSIRG.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+/*
+//Q1
+int main()
+{
+    int x;
+    printf("Enter a three digit number");
+    scanf("%d",&x);
+    printf("Sum is %d",x/10/10+x/10%10+x%10);
+    printf("\n");
+    return 0;
+}
+//Q2
+int main()
+{
+    char x='+';
+    printf("ASCII of %c is %d",x,x);
+    printf("\n");
+    return 0;
+}
+*/
+//Q3
+int main()
+{
+    char x;
+    int y;
+    float z;
+    double w;
+
+    printf("%d %d %d %d",sizeof(x),sizeof(y),sizeof(z),sizeof(w));
+    printf("\n");
+    return 0;
+}
+/*
+//Q4
+int main()
+{
+    int x;
+    printf("Enter a digit number");
+    scanf("%d",&x);
+    printf("Result is %d",x/10*10);
+    printf("\n");
+    return 0;
+}
+//Q5
+int main()
+{
+    int num,digit;
+    printf("Enter a number");
+    scanf("%d",&num);
+    printf("Enter a digit");
+    scanf("%d",&digit);
+    num=num*10+digit;
+    printf("Result is %d",num);
+    printf("\n");
+    return 0;
+}
+*/
