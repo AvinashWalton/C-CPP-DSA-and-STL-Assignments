a237b859743dd8800c2c3b1130510774b494875d
Sonu Suman <avinashwalton@gmail.com>
2024-03-22 21:44:51 +0530

    Assignments

--- C Language/8. Assignments - More on Decision Control/5.c
+++ C Language/8. Assignments - More on Decision Control/5.c
@@ -8,13 +8,13 @@ int main()
     printf("Enter three numbers: ");
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    if(num1>num2 || num1>num3)
+    if(num1>num2 && num1>num3)
     printf("Greatest number is %d\n", num1);
 
-    else if(num2>num1 || num2>num3)
+    else if(num2>num1 && num2>num3)
     printf("Greatest number is %d\n", num2);
 
-    else if(num3>num1 || num3>num2)
+    else if(num3>num1 && num3>num2)
     printf("Greatest number is %d\n", num3);
 
     else
