a20e3e2de0cbaae5aeaec2918db1d537248fea86
Sonu Suman <sonukumarsuman081@gmail.com>
2024-04-04 23:07:10 +0530

    MySirG Assignments Solution

---
+++ C Language/12. Assignments - while loop/Assign12_Solution_MYSIRG.c
@@ -0,0 +1,141 @@
+#include<stdio.h>
+//Q1
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q2
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q3
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",n+1-i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q4
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",2*i-1);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q5
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",2*n-2*i+1);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q6
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",2*i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q7
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",2*n+2-2*i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q8
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",i*i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q9
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d ",i*i*i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+//Q10
+int main()
+{
+    int i=1,n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    while(i<=10)
+    {
+        printf("%d x %d = %d\n",n,i,n*i);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
